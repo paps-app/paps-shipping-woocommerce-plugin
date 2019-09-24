bfeb1191944d43d115512111c76a13999527f52b
madiodio <papigaye1@gmail.com>
2019-09-24 15:14:56 +0000

    1.2.10

--- paps-shipping-for-woocommerce.php
+++ paps-shipping-for-woocommerce.php
@@ -2,7 +2,7 @@
 /*
 	Plugin Name: Paps Shipping for WooCommerce
 	Description: Paps Shipping & Delivery Tracking Integration for WooCommerce
-	Version: 1.2.9
+	Version: 1.2.10
 	Author: Paps
 	Author URI: www.paps.sn
 */
